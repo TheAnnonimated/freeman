bd0ad2812b02883cbc989490b2d95616564d38da
TheAnnonimated <nathanmatosbispo@gmail.com>
2025-10-12 22:30:43 -0300

    Atualização premiumUsers via bot

--- veri.lua
+++ veri.lua
@@ -7,7 +7,6 @@ local premiumUsers = {
     ["xes_jsok"] = true,
     ["Itz_Mariena"] = true,
     ["thiagojuniorgamer12"] = true
-    ["Web_Julio9"] = true,
 }
 
 local gui = Instance.new("ScreenGui")
