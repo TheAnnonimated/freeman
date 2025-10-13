e2698ddb151da706a9c100f3292894322445de1a
TheAnnonimated <nathanmatosbispo@gmail.com>
2025-10-13 14:04:20 -0300

    Atualização da lista premium via bot

--- veri.lua
+++ veri.lua
@@ -2,11 +2,10 @@ local player = game:GetService("Players").LocalPlayer
 
 local premiumUsers = {
     ["JumexD24"] = true,
-    ["Kaua_452"] = true,
     ["pedro312jee"] = true,
     ["xes_jsok"] = true,
     ["Itz_Mariena"] = true,
-    ["thiagojuniorgamer12"] = true
+    ["thiagojuniorgamer12"] = true,
 }
 
 local gui = Instance.new("ScreenGui")
